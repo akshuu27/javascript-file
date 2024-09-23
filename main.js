d375b00397b6fec647be538f244b56dec633dc4d
akshuu27 <akshujagdale2003@gmail.com>
2024-09-23 19:19:27 +0530

    main commit

---
+++ main.js
@@ -0,0 +1,9 @@
+// import{add , sub , mult ,div } from "./mymodule.js"
+
+// const a= 10;
+// const b= 20;
+
+// console.log("addtion :"+ add(a,b)+ "<br>");
+// console.log("substraction :"+ add(a,b)+ "<br>"););
+// console.log("multiplication:"+ add(a,b)+ "<br>"););
+// console.log("division:"+ add(a,b)+ "<br>"););
